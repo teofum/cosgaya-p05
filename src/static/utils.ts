ce31ba51bec0f83d1d659c086a69a9331066f162
Teo Fumagalli <teo.fum@gmail.com>
2021-09-28 10:44:32 -0300

    This is not an actual branch, it's used for hosting a build on GitHub Pages

--- src/static/utils.ts
+++
@@ -1,7 +0,0 @@
-export function setContains<T> (a: T[], b: T[]): boolean {
-  return b.every(i => a.includes(i));
-}
-
-export function setEquals<T> (a: T[], b: T[]): boolean {
-  return a.every(i => b.includes(i)) && b.every(i => a.includes(i));
-}
